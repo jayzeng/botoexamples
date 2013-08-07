7a29bd972ecc0a523a01627fc57e61170a1c3a2f
Jay Zeng <jayzeng@jay-zeng.com>
2013-08-07 11:48:42 -0700

    * Move util.py to config * Make connect_to_s3 independent

--- s3/connect_to_s3.py
+++ s3/connect_to_s3.py
@@ -1,8 +1,7 @@
-import conifg
+from boto.s3.connection import S3Connection
 
 # Description:
 # How to create a s3 bucket
 
-from boto.s3.connection import S3Connection
-s3_conn = S3Connection(aws_key, aws_secret)
+s3_conn = S3Connection('aws_key', 'aws_secret')
 bucket = conn.create_bucket(s3_conn)
