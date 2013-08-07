4b6a3a5b68af807b89ad00551275c17789fc2ba2
Jay Zeng <jayzeng@jay-zeng.com>
2013-08-07 11:42:38 -0700

    * Prototype a script to extract description fields

---
+++ s3/util.py
@@ -0,0 +1,6 @@
+from boto import Config as BotoConfig
+
+boto_config = BotoConfig()
+
+aws_key     = boto_config.get('Credentials', 'aws_access_key_id')
+aws_secret  = boto_config.get('Credentials', 'aws_secret_access_key')
