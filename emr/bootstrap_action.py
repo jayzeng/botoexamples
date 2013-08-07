680b3ba5d38b721f5855744cfc307856fe55d034
Nick Rushton <nick@hasoffers.com>
2013-08-07 17:59:50 +0000

    adding code example for emr bootstrap action

---
+++ emr/bootstrap_action.py
@@ -0,0 +1,17 @@
+"""
+BootstrapAction is an object reperesenting a bootstrap action in Elastic Map
+Reduce (EMR), a script that gets run before the EMR job executes.
+"""
+from boto.emr.bootstrap_action import BootstrapAction
+from boto.emr.connection import EmrConnection
+
+# initialize a bootstrap action
+bootstrapSetup = BootstrapAction("Bootstrap Name",
+                                 "s3://<my-bucket>/<my-bootstrap-action>",
+                                 ["arg1=hello", "arg2=world"])
+
+# initialize emr connection
+emr_job = EmrConnection("<aws-access-key-id>", "<aws-secret-access-key>")
+
+# run emr job flow with defined bootstrap action
+emr_job.run_jobflow(bootstrap_actions=[bootstrapSetup])
